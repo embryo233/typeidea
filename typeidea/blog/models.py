70b0ef5c3e6228a287f14bd96f71b0c906897d9f
embryo <embryo233@gmail.com>
2020-07-09 15:08:50 +0000

    封装侧边栏逻辑

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -132,6 +132,6 @@ class Post(models.Model):
     def hot_posts(cls):
         result = cache.get('hot_posts')
         if not result:
-            result = cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-pv')
+            result = cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-pv').only('id','title')
             cache.set('hot_posts', result, 10 * 60)
         return result
