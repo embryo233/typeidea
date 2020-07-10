9ed0c58a81409c9b1de801f294df61586076a7d4
embryo <embryo233@gmail.com>
2020-07-10 03:46:14 +0000

    升级至class-based view

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,9 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.views.generic import DetailView,ListView
 
 from .models import Post,Tag,Category
 from config.models import SideBar
 # Create your views here.
+
+class CommonViewMixin:
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        context.update({
+            'sidebars':SideBar.get_all(),
+        })
+        context.update(Category.get_navs())
+        return context
+
+class IndexView(CommonViewMixin,ListView):
+    queryset=Post.latest_posts()
+    paginate_by=5
+    context_object_name='post_list'
+    template_name='blog/list.html'
+
+class PostListView(ListView):
+    queryset=Post.latest_posts()
+    paginate_by=1
+    context_object_name='post_list'
+    template_name='blog/list.html'
 
 def post_list(request,category_id=None,tag_id=None):
     tag=None
@@ -25,6 +47,12 @@ def post_list(request,category_id=None,tag_id=None):
     context.update({'sidebars':SideBar.get_all()})
     return render(request,'blog/list.html',context=context)
 
+class PostDetailView(CommonViewMixin,DetailView):
+    queryset=Post.latest_posts()
+    template_name='blog/detail.html'
+    context_object_name='post'
+    pk_url_kwarg='post_id'
+
 def post_detail(request,post_id):
     try:
         post=Post.objects.get(id=post_id)
@@ -34,3 +62,35 @@ def post_detail(request,post_id):
     context.update(Category.get_navs())
     context.update({'sidebars':SideBar.get_all()})
     return render(request,'blog/detail.html',context=context)
+
+class CategoryView(IndexView):
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        category_id=self.kwargs.get('category_id')
+        category=get_object_or_404(Category,pk=category_id)
+        context.update({
+            'category':category,
+        })
+        return context
+
+    def get_queryset(self):
+        '''重写queryset，根据分类过滤'''
+        queryset=super().get_queryset()
+        category_id=self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+class TagView(IndexView):
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        tag_id=self.kwargs.get('tag_id')
+        tag=get_object_or_404(Tag,pk=tag_id)
+        context.update({
+            'tag':tag,
+        })
+        return context
+
+    def get_queryset(self):
+        '''重写queryset，根据分类过滤'''
+        queryset=super().get_queryset()
+        tag_id=self.kwargs.get('tag_id')
+        return queryset.filter(tag__id=tag_id)
