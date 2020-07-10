389f800807b4c8acc2c3b9cb6809ba48aaa18972
embryo <embryo233@gmail.com>
2020-07-10 15:45:48 +0000

    增加评论模块

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -22,6 +22,7 @@ from blog.views import (
     PostDetailView,SearchView,AuthorView,
 )
 from config.views import LinkListView
+from comment.views import CommentView
 from .custom_site import custom_site
 
 urlpatterns = [
@@ -34,4 +35,5 @@ urlpatterns = [
     path('search/',SearchView.as_view(),name='search'),
     path('author/<int:owner_id>/',AuthorView.as_view(),name='author'),
     path('links/',LinkListView.as_view(),name='links'),
+    path('comment/',CommentView.as_view(),name='comment'),
 ]
