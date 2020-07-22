2a4ccf40148d510898a3eb841edf7c74a4a2da4e
embryo <embryo233@gmail.com>
2020-07-22 09:06:10 +0000

    配置MySQL

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -6,9 +6,18 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'typeidea_db',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        # 'OPTIONS': {'charset': 'utf8mb4'}
     }
 }
 
